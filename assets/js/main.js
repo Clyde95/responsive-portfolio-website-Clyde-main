fdf5f8ebfb12571e904ba7bf20fd5b6e26445ff2
Clyde95 <clydekambamura@icloud.com>
2023-09-05 14:05:23 +0200

    Contacts done without the emailjs configured

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,7 +43,33 @@ const shadowHeader = () =>{
 window.addEventListener('scroll', shadowHeader)
 /*=============== EMAIL JS ===============*/
 
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    //serviceID - templateID - #form - publicKey
+    emailjs.sendForm('', '', '', '')
+    .then(() =>{
+        //show sent message
+        contactMessage.textContent='Message sent successfully '
+
+        //remove message after 5 seconds
+        setTimeout(() => {
+            contactMessage.textContent= ''
+
+        }, 5000)
+
+        //Clear input fields
+        contactForm.reset()
+    },  () =>{
+        //show error message
+        contactMessage.textContent= 'Message not sent (service error) '
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
 
 
